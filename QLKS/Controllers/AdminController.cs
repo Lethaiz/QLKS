d4381b0dd8e3ebb9aee4b6f5206ea24f7c46a65e
Lethaiz <nhokthai999@gmail.com>
2021-11-16 19:40:05 +0700

    admin fix

---
+++ QLKS/Controllers/AdminController.cs
@@ -0,0 +1,67 @@
+﻿using QLKS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLKS.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbQLKSDataContext data = new dbQLKSDataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến 
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                //Gán giá trị cho đối tượng được tạo mới (ad)        
+
+                Admin ad = data.Admins.SingleOrDefault(n => n.TenDangNhap == tendn && n.MatKhau == matkhau);
+                if (ad != null)
+                {
+                    // ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+
+
+        public ActionResult DateTime()
+        {
+            return View();
+        }
+    }
+}
