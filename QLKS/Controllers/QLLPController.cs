375ed3d09c2a215e7e8f6bc0715d7c6e4728a876
Lethaiz <nhokthai999@gmail.com>
2021-11-17 19:11:53 +0700

    admin hoan chinh

--- QLKS/Controllers/QLLPController.cs
+++ QLKS/Controllers/QLLPController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PagedList;
+using QLKS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,108 @@ namespace QLKS.Controllers
     public class QLLPController : Controller
     {
         // GET: QLLP
-        public ActionResult Index()
+        dbQLKSDataContext data = new dbQLKSDataContext();
+
+
+        public ActionResult Loaiphong(int? page)
         {
-            return View();
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                int pageNumber = (page ?? 1);
+                int pageSize = 7;
+                //return View(db.SACHes.ToList());
+                return View(data.LoaiPhongs.ToList().OrderBy(n => n.MaLP).ToPagedList(pageNumber, pageSize));
+            }
+
+        }
+        public ActionResult ChiTietLP(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+            {
+                var l = from n in data.LoaiPhongs where n.MaLP == id select n;
+                return View(l.SingleOrDefault());
+            }
+        }
+        //3. Thêm mới Nhà xuất bản
+        [HttpGet]
+        public ActionResult themloai()
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+                return View();
+        }
+        [HttpPost]
+        public ActionResult themloai(LoaiPhong l)
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+            {
+                data.LoaiPhongs.InsertOnSubmit(l);
+                data.SubmitChanges();
+
+                return RedirectToAction("Loaiphong", "Loaiphong");
+            }
+        }
+        //4. Xóa 1 Nhà xuất bản gồm 2 trang: xác nhận xóa và xử lý xóa
+        [HttpGet]
+        public ActionResult xoaloai(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+            {
+                var l = from nxb in data.LoaiPhongs where nxb.MaLP == id select nxb;
+                return View(l.SingleOrDefault());
+            }
+        }
+        [HttpPost, ActionName("xoaloai")]
+        public ActionResult Xacnhanxoa(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+            {
+                LoaiPhong l = data.LoaiPhongs.SingleOrDefault(n => n.MaLP == id);
+                data.LoaiPhongs.DeleteOnSubmit(l);
+                data.SubmitChanges();
+
+                return RedirectToAction("Loaiphong", "Loaiphong");
+            }
+        }
+        //5. Điều chỉnh thông tin 1  Nhà xuất bản gồm 2 trang: Xem và điều chỉnh và cập nhật lưu lại
+        [HttpGet]
+        public ActionResult Sualoai(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+            {
+                var l = from nxb in data.LoaiPhongs where nxb.MaLP == id select nxb;
+                return View(l.SingleOrDefault());
+            }
+        }
+        [HttpPost, ActionName("Sualoai")]
+        public ActionResult Capnhat(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("Login", "Admin");
+            else
+            {
+                LoaiPhong l = data.LoaiPhongs.SingleOrDefault(n => n.MaLP == id);
+
+                UpdateModel(l);
+                data.SubmitChanges();
+                return RedirectToAction("Loaiphong", "Loaiphong");
+            }
         }
     }
 }
--- QLKS/Controllers/QLPHONGController.cs
+++ QLKS/Controllers/QLPHONGController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PagedList;
+using QLKS.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +12,213 @@ namespace QLKS.Controllers
     public class QLPHONGController : Controller
     {
         // GET: QLPHONG
-        public ActionResult Index()
+        dbQLKSDataContext data = new dbQLKSDataContext();
+        public ActionResult QLPhong(int? page)
         {
-            return View();
+    
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                int pageNumber = (page ?? 1);
+                int pageSize = 7;
+                //return View(db.SACHes.ToList());
+                return View(data.Phongs.ToList().OrderBy(n => n.MaPhong).ToPagedList(pageNumber, pageSize));
+            }
+
+        }
+        public ActionResult ThemmoiSanpham()
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                ViewBag.MALOAI = new SelectList(data.LoaiPhongs.ToList().OrderBy(n => n.TenLP), "MALOAI", "TENLOAI");
+                ViewBag.MAKHVUC = new SelectList(data.KHUVUCs.ToList().OrderBy(n => n.TENKV), "MAKHVUC", "TENKV");
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemmoiSanpham(Phong p, HttpPostedFileBase fileUpload)
+        {
+            //Dua du lieu vao dropdownload
+            ViewBag.MALOAI = new SelectList(data.LoaiPhongs.ToList().OrderBy(n => n.TenLP), "MALOAI", "TENLOAI");
+            ViewBag.MAKHVUC = new SelectList(data.KHUVUCs.ToList().OrderBy(n => n.TENKV), "MAKHVUC", "TENKV");
+            //Kiem tra duong dan file
+            if (fileUpload == null)
+            {
+                ViewBag.Thongbao = "Vui lòng chọn ảnh bìa";
+                return View();
+            }
+            //Them vao CSDL
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var fileName = Path.GetFileName(fileUpload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/img"), fileName);
+                    if (System.IO.File.Exists(path))
+                        ViewBag.Thongbao = "Hình ảnh đã tồn tại";
+                    else
+                    {
+                        fileUpload.SaveAs(path);
+                    }
+                    p.ANH= fileName;
+                    data.Phongs.InsertOnSubmit(p);
+                    data.SubmitChanges();
+                }
+                return RedirectToAction("QLPhong");
+            }
+        }
+
+        //Hiển thị sản phẩm
+        public ActionResult ChitietSanpham(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                //Lay ra doi tuong sach theo ma
+                Phong sp = data.Phongs.SingleOrDefault(n => n.MaPhong == id);
+                ViewBag.Masach = sp.MaPhong;
+                if (sp == null)
+                {
+                    Response.StatusCode = 404;
+                    return null;
+                }
+                return View(sp);
+            }
+        }
+
+        //Xóa sản phẩm
+        [HttpGet]
+        public ActionResult XoaSanpham(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                //Lay ra doi tuong sach can xoa theo ma
+                Phong sp = data.Phongs.SingleOrDefault(n => n.MaPhong == id);
+                ViewBag.Masach = sp.MaPhong;
+                if (sp == null)
+                {
+                    Response.StatusCode = 404;
+                    return null;
+                }
+                return View(sp);
+            }
+        }
+
+
+        [HttpPost, ActionName("XoaSanpham")]
+        public ActionResult Xacnhanxoa(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                //Lay ra doi tuong sach can xoa theo ma
+                Phong sp = data.Phongs.SingleOrDefault(n => n.MaPhong == id);
+                ViewBag.MASP = sp.MaPhong;
+                if (sp == null)
+                {
+                    Response.StatusCode = 404;
+                    return null;
+                }
+                data.Phongs.DeleteOnSubmit(sp);
+                data.SubmitChanges();
+                return RedirectToAction("QLPhong");
+            }
+        }
+        //Chinh sửa sản phẩm
+        [HttpGet]
+        public ActionResult Suasanpham(int id)
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                //Lay ra doi tuong sach theo ma
+                Phong sp = data.Phongs.SingleOrDefault(n => n.MaPhong == id);
+                ViewBag.MASP = sp.MaPhong;
+                if (sp == null)
+                {
+                    Response.StatusCode = 404;
+                    return null;
+                }
+
+                ViewBag.MALOAI = new SelectList(data.LoaiPhongs.ToList().OrderBy(n => n.TenLP), "MALOAI", "TENLOAI");
+                ViewBag.MAKHVUC = new SelectList(data.KHUVUCs.ToList().OrderBy(n => n.TENKV), "MAKHVUC", "TENKV");
+                return View(sp);
+            }
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Suasanpham(int id, HttpPostedFileBase fileUpload)
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+
+            }
+            else
+            {
+                //Dua du lieu vao dropdownload
+                ViewBag.MALOAI = new SelectList(data.LoaiPhongs.ToList().OrderBy(n => n.TenLP), "MALOAI", "TENLOAI");
+                ViewBag.MAKHVUC = new SelectList(data.KHUVUCs.ToList().OrderBy(n => n.TENKV), "MAKHVUC", "TENKV");
+                //Kiem tra duong dan file
+                if (fileUpload == null)
+                {
+                    ViewBag.Thongbao = "Vui lòng chọn ảnh bìa";
+                    return View();
+                }
+                //Them vao CSDL
+                else
+                {
+                    Phong sp = data.Phongs.SingleOrDefault(n => n.MaPhong == id);
+                    if (ModelState.IsValid)
+                    {
+                        //Luu ten fie, luu y bo sung thu vien using System.IO;
+                        var fileName = Path.GetFileName(fileUpload.FileName);
+                        //Luu duong dan cua file
+                        var path = Path.Combine(Server.MapPath("~/Content/img"), fileName);
+                        //Kiem tra hình anh ton tai chua?
+                        if (System.IO.File.Exists(path))
+                            ViewBag.Thongbao = "Hình ảnh đã tồn tại";
+                        else
+                        {
+                            //Luu hinh anh vao duong dan
+                            fileUpload.SaveAs(path);
+                        }
+                        sp.ANH = fileName;
+                        //Luu vao CSDL   
+                        UpdateModel(sp);
+                        data.SubmitChanges();
+
+                    }
+                    return RedirectToAction("QLPhong");
+                }
+            }
         }
     }
 }
